295366b2220aafa230a179f0539da9803f1be597
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-03 16:10:05 +0200

    This time it actually works

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,29 @@
-
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
- // Consult https://github.com/sveltejs/svelte-preprocess
- // for more information about preprocessors
- preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
- kit: {
- // hydrate the <div id="svelte"> element in src/app.html
-  adapter: adapter({
-    pages: 'build',
-    assets: 'build',
-    fallback: null
-  }),
-  paths: {
-	// change below to your repo name
-	base: dev ? "" : "/svelte-personal-website",
-},
- }
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: dev ? '' : '/svelte-personal-website',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'app_',
+	}
 };
 
 export default config;
