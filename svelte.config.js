10832439442cb88b029337a2ecada4ac888d15c1
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-03 15:11:46 +0200

    GitHub Pages changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+const dev = process.env.NODE_ENV === 'development';
 
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+            // change below to your repo name
+            base: dev ? "" : "/svelte-personal-website",
+		},
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
-
-export default config;
