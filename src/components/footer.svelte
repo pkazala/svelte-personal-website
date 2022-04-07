5c3629ba9d913c61474056c6e381384e4158fd98
Piotrek <piotr.kazala.2020@uni.strath.ac.uk>
2022-04-07 16:04:45 +0200

    Changed LinkedIn url

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,7 +2,7 @@
     <h1 class="text-2xl mb-4 font-bold font-mono dark:text-gray-100 text-gray-600">My Socials</h1>
         <div class="h-fit pb-16 max-w-xs grid grid-cols-2 justify-items-center m-auto">
             <a href="https://github.com/pkazala" target="_blank" class="h-12 w-12 bg-cover bg-light-gh hover:bg-light-gh-h dark:bg-dark-gh dark:hover:bg-dark-gh-h cursor-pointer"></a>
-            <a href="https://www.linkedin.com/in/piotr-kaza%C5%82a-080274213/" target="_blank" class="h-12 w-12 bg-cover bg-light-linked hover:bg-light-linked-h dark:bg-dark-linked dark:hover:bg-dark-linked-h cursor-pointer"></a>
+            <a href="https://www.linkedin.com/in/piotrkazala/" target="_blank" class="h-12 w-12 bg-cover bg-light-linked hover:bg-light-linked-h dark:bg-dark-linked dark:hover:bg-dark-linked-h cursor-pointer"></a>
     </div>
     <h1 class="text-gray-600 dark:text-gray-300">This website was built using:</h1>
     <p class="font-light text-gray-600 dark:text-gray-300">TypeScript | Svelte | Tailwind | Formspree</p>
