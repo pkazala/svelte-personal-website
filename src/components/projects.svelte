9160333b9561ffe580b69b1ed870653db0781c91
Piotrek <pepe.kazala@gmail.com>
2022-04-26 12:36:23 +0200

    Added animations to projects

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -2,8 +2,8 @@
 	<h1 class="text-2xl mb-3 font-bold font-mono dark:text-gray-100 text-gray-600">Projects</h1>
 	<div class="grid mine:grid-cols-2 grid-cols-1 my-5 p-3 items-center">
 		<div class="grid grid-cols-1 sm:p-3 sm:pr-5">
-			<h1 class="text-4xl font-mono font-bold text-gray-600 dark:text-gray-100">E-Commerce website</h1>
-			<div class="text-2xl mt-4 text-gray-500 dark:text-gray-300">
+			<h1 data-aos="fade-up" data-aos-easing="ease-in-out" class="text-4xl font-mono font-bold text-gray-600 dark:text-gray-100">E-Commerce website</h1>
+			<div data-aos="fade-up" data-aos-delay=300 class="text-2xl mt-4 text-gray-500 dark:text-gray-300 text-left">
 				An E-Commerce website built with a combination of Vue and Tailwind on the Front-End and
 				Python (Flask) and MongoDB Atlas on the server-side. I decided to build this project to test
 				my Full-Stack development skills and learn a couple new technologies. The website includes a
@@ -11,10 +11,12 @@
 				Flask server, Authentication and Authorisation using JWT as well as Stripe to handle the
 				payments.
 			</div>
-			<p class="font-mono mt-10 text-xl text-gray-600 dark:text-gray-300">Technologies used:</p>
-			<p class="font-light text-lg text-gray-600 dark:text-gray-300">Vue (Vuex) | Tailwind | Flask | MongoDB Atlas | Stripe</p>
-			<p class="font-mono mt-4 text-xl text-gray-600 dark:text-gray-300">Links:</p>
-			<p class="font-mono text-gray-600 dark:text-gray-300"><a href="https://github.com/pkazala/vue-shop" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub (Front-End)</a> | <a href="https://github.com/pkazala/flask-server" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub (Server)</a> | <a href="https://vue-shop-app.netlify.app/" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">Live Demo</a></p>
+			<div data-aos="fade-up" data-aos-delay=300>
+				<p class="font-mono mt-10 text-xl text-gray-600 dark:text-gray-300">Technologies used:</p>
+				<p class="font-light text-lg text-gray-600 dark:text-gray-300">Vue (Vuex) | Tailwind | Flask | MongoDB Atlas | Stripe</p>
+				<p class="font-mono mt-4 text-xl text-gray-600 dark:text-gray-300">Links:</p>
+				<p class="font-mono text-gray-600 dark:text-gray-300"><a href="https://github.com/pkazala/vue-shop" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub (Front-End)</a> | <a href="https://github.com/pkazala/flask-server" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub (Server)</a> | <a href="https://vue-shop-app.netlify.app/" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">Live Demo</a></p>
+			</div>
 		</div>
 		<div class="mockup-window border dark:bg-gray-200 bg-base-300 mt-4 mine:max-h-80 lg:max-h-fit">
 			<img src='../Assets/final_gif.gif' alt="pimpaczek" class="h-content" />
@@ -25,18 +27,20 @@
 			<img src="../Assets/second_gif.gif" alt="pimpaczek" class="h-content" />
 		</div>
 		<div class="grid grid-cols-1 sm:p-3 sm:pr-5">
-			<h1 class="text-4xl font-mono font-bold text-gray-600 dark:text-gray-100">Bank Application</h1>
-			<div class="text-2xl mt-4 text-gray-600 dark:text-gray-300">
+			<h1 data-aos="fade-up" data-aos-easing="ease-in-out" class="text-4xl font-mono font-bold text-gray-600 dark:text-gray-100">Bank Application</h1>
+			<div data-aos="fade-up" data-aos-delay=300 class="mine:pl-5 text-2xl mt-4 text-gray-600 dark:text-gray-300 text-left">
 				This application was built in a group of 5 during my University Course in Agile Software
 				Development in Practice. We worked in sprints doing regular stand-ups and utilising Agile
 				tools like Trello. The application interface was built with simple tools as it only utilised
 				HTML, CSS and JavaScript (a bit of jQuery) and the REST API was built with Jooby, a Java
 				Framework. My duties included building out the Front-End and Unit-Testing.
 			</div>
-			<p class="font-mono mt-10 text-xl text-gray-600 dark:text-gray-300">Technologies used:</p>
-			<p class="font-light text-lg text-gray-600 dark:text-gray-300">HTML | CSS | JS (jQuery) | Java (Jooby) | SQL</p>
-			<p class="font-mono mt-4 text-xl text-gray-600 dark:text-gray-300">Links:</p>
-			<p class="font-mono text-gray-600 dark:text-gray-300"><a href="https://github.com/pkazala/Bank-Application" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub</a></p>
+			<div data-aos="fade-up" data-aos-delay=300>
+				<p class="font-mono mt-10 text-xl text-gray-600 dark:text-gray-300">Technologies used:</p>
+				<p class="font-light text-lg text-gray-600 dark:text-gray-300">HTML | CSS | JS (jQuery) | Java (Jooby) | SQL</p>
+				<p class="font-mono mt-4 text-xl text-gray-600 dark:text-gray-300">Links:</p>
+				<p class="font-mono text-gray-600 dark:text-gray-300"><a href="https://github.com/pkazala/Bank-Application" target="_blank" class="underline hover:text-cyan-500 dark:hover:text-rose-400">GitHub</a></p>
+			</div>
 		</div>
 	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,28 @@
 <script>
-    import Nav from '../components/nav.svelte';
-    import Header from '../components/header.svelte';
-    import Technologies from '../components/technologies.svelte';
-    import Projects from '../components/projects.svelte';
-    import Contact from '../components/contact.svelte';
-    import Footer from '../components/footer.svelte';
+	import Nav from '../components/nav.svelte';
+	import Header from '../components/header.svelte';
+	import Technologies from '../components/technologies.svelte';
+	import Projects from '../components/projects.svelte';
+	import Contact from '../components/contact.svelte';
+	import Footer from '../components/footer.svelte';
+	import AOS from 'aos';
+    import 'aos/dist/aos.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		AOS.init({
+			once: false,
+            delay: 100,
+            duration: 500
+		})
+    })
 </script>
 
 <div class="dark:bg-gray-800 bg-stone-50">
-    <Nav></Nav>
-    <Header></Header>
-    <Technologies></Technologies>
-    <Projects></Projects>
-    <Contact></Contact>
-    <Footer></Footer>
+	<Nav />
+	<Header />
+	<Technologies />
+	<Projects />
+	<Contact />
+	<Footer />
 </div>
