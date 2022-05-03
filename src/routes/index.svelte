dc51ca3b817964240ea843188f12b2f0b5ef61f3
Piotrek <pepe.kazala@gmail.com>
2022-05-03 13:52:32 +0200

    Fixed scrolling on cursor

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,10 +17,7 @@
 		});
 		const cursor = document.querySelector('.cursor');
 		document.addEventListener('mousemove', (event) => {
-			cursor.setAttribute(
-				'style',
-				'top: ' + (event.pageY - 8) + 'px; left: ' + (event.pageX - 8) + 'px;'
-			);
+			cursor.setAttribute("style","top: "+(event.pageY - scrollY)+"px; left: "+(event.pageX)+"px");
 
 			document.addEventListener('click', () => {
 				cursor.classList.remove('shrink');
