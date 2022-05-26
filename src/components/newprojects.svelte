5a86886af8227338d227f2baf75e74f21c9edffa
Piotrek <pepe.kazala@gmail.com>
2022-05-26 19:57:07 +0100

    hopefully this fixes it

--- src/components/newprojects.svelte
+++ src/components/newprojects.svelte
@@ -75,14 +75,13 @@
 	class="h-fit mt-44 max-w-7xl m-auto grid grid-cols-1 font-stylish text-black dark:text-white px-10 md:px-12 mine:px-14 lg:px-18 xlg:px-24 xl:px-[7.5rem]"
 >
 	<h1 data-aos="fade-up" class="text-5xl justify-self-end z-10">Projects</h1>
-	<img src='../Assets/final_gif.gif' alt="pimpaczek" class="h-content" />
 	<div class="divider bg-black dark:bg-white  h-[1px] opacity-20" />
 	<ul class="font-stylish text-4xl" on:mousemove={handleMousemove}>
 		<li data-aos="fade-up" class="group mt-20 hlink grid grid-cols-2" on:mouseenter={() => visible1 = true} on:mouseleave={() => visible1 = false} on:click={handleClick1}>
 			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Vue Store</h1>
 			{#if visible1 && !section1}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src="../static/Assets/photos/project1.jpg" alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img src='../Assets/photos/project1.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
 			{/if}
 		</li>
 			{#if section1}
@@ -96,7 +95,7 @@
 			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Java Banking App</h1>
 			{#if visible2 && !section2}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src="../static/Assets/photos/project2.jpg" alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img src='../Assets/photos/project2.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
 			{/if}
 		</li>
 			{#if section2}
@@ -110,7 +109,7 @@
 			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Portfolio Website</h1>
 			{#if visible4 && !section4}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src="../static/Assets/photos/project4.jpg" alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img src='../Assets/photos/project4.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
 			{/if}
 		</li>
 			{#if section4}
