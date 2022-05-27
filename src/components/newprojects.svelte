bffb14ccef5c18c16dc9d33192bd8769d779ed5c
Piotrek <pepe.kazala@gmail.com>
2022-05-27 19:56:18 +0100

    Added smooth scroll when closing projects

--- src/components/newprojects.svelte
+++ src/components/newprojects.svelte
@@ -19,7 +19,7 @@
 	function handleMousemove(e) {
 		top = e.clientY;
 		left = e.clientX;
-	};
+	}
 
 	function handleClick1() {
 		if (section1) {
@@ -27,7 +27,7 @@
 		} else {
 			section1 = true;
 		}
-	};
+	}
 
 	function handleClick2() {
 		if (section2) {
@@ -35,7 +35,7 @@
 		} else {
 			section2 = true;
 		}
-	};
+	}
 
 	function handleClick3() {
 		if (section3) {
@@ -43,7 +43,7 @@
 		} else {
 			section3 = true;
 		}
-	};
+	}
 
 	function handleClick4() {
 		if (section4) {
@@ -51,7 +51,16 @@
 		} else {
 			section4 = true;
 		}
-	};
+	}
+
+	function scrollIntoView({ target }) {
+		const el = document.querySelector(target.getAttribute('href'));
+		if (!el) return;
+		el.scrollIntoView({
+			behavior: 'smooth',
+			duration: 1000
+		});
+	}
 
 	onMount(() => {
 		const cursor = document.querySelector('.cursor');
@@ -77,47 +86,119 @@
 	<h1 data-aos="fade-up" class="text-5xl justify-self-end z-10">Projects</h1>
 	<div class="divider bg-black dark:bg-white  h-[1px] opacity-20" />
 	<ul class="font-stylish text-4xl" on:mousemove={handleMousemove}>
-		<li data-aos="fade-up" class="group mt-20 hlink grid grid-cols-2" on:mouseenter={() => visible1 = true} on:mouseleave={() => visible1 = false} on:click={handleClick1}>
-			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Vue Store</h1>
+		<li
+			data-aos="fade-up"
+			class="group mt-20 hlink grid grid-cols-2"
+			on:mouseenter={() => (visible1 = true)}
+			on:mouseleave={() => (visible1 = false)}
+			on:click={handleClick1}
+		>
+			<h1 id="section-1" class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">
+				Vue Store
+			</h1>
 			{#if visible1 && !section1}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src='../Assets/photos/project1.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img
+					src="../Assets/photos/project1.jpg"
+					alt="project-1"
+					class="w-[275px] image rounded-lg"
+					style="top: {top}px; left: {left}px;"
+					transition:fade={{ delay: 150 }}
+				/>
 			{/if}
 		</li>
-			{#if section1}
-				<div class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn" transition:slide>
-					<Section1/>
-					<button on:click={() => section1 = false} class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300">Hide</button>
-				</div>
-			{/if}
+		{#if section1}
+			<div
+				class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn"
+				in:slide
+				out:slide={{ delay: 600, duration: 700 }}
+			>
+				<Section1 />
+				<a
+					href="#section-1"
+					on:click|preventDefault={scrollIntoView}
+					on:click={() => (section1 = false)}
+					class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300"
+					>Hide</a
+				>
+			</div>
+		{/if}
 		<div class="divider bg-black dark:bg-white h-[1px] opacity-20" />
-		<li data-aos="fade-up" class="group mt-20 hlink grid grid-cols-2" on:mouseenter={() => visible2 = true} on:mouseleave={() => visible2 = false} on:click={handleClick2}>
-			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Java Banking App</h1>
+		<li
+			data-aos="fade-up"
+			class="group mt-20 hlink grid grid-cols-2"
+			on:mouseenter={() => (visible2 = true)}
+			on:mouseleave={() => (visible2 = false)}
+			on:click={handleClick2}
+		>
+			<h1 id="section-2" class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">
+				Java Banking App
+			</h1>
 			{#if visible2 && !section2}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src='../Assets/photos/project2.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img
+					src="../Assets/photos/project2.jpg"
+					alt="project-1"
+					class="w-[275px] image rounded-lg"
+					style="top: {top}px; left: {left}px;"
+					transition:fade={{ delay: 150 }}
+				/>
 			{/if}
 		</li>
-			{#if section2}
-				<div class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn" transition:slide>
-					<Section2/>
-					<button on:click={() => section2 = false} class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300">Hide</button>
-				</div>
-			{/if}
+		{#if section2}
+			<div
+				class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn"
+				in:slide
+				out:slide={{ delay: 600, duration: 700 }}
+			>
+				<Section2 />
+				<a
+					href="#section-2"
+					on:click|preventDefault={scrollIntoView}
+					on:click={() => (section2 = false)}
+					class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300"
+					>Hide</a
+				>
+			</div>
+		{/if}
 		<div class="divider bg-black dark:bg-white h-[1px] opacity-20" />
-		<li data-aos="fade-up" class="group mt-20 hlink grid grid-cols-2" on:mouseenter={() => visible4 = true} on:mouseleave={() => visible4 = false} on:click={handleClick4}>
-			<h1 class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">Portfolio Website</h1>
+		<li
+			data-aos="fade-up"
+			class="group mt-20 hlink grid grid-cols-2"
+			on:mouseenter={() => (visible4 = true)}
+			on:mouseleave={() => (visible4 = false)}
+			on:click={handleClick4}
+		>
+			<h1 id="section-4" class="group-hover:text-gray-500 transition ease-in-out duration-300 z-10">
+				Portfolio Website
+			</h1>
 			{#if visible4 && !section4}
 				<p class="justify-self-end z-10" transition:fade>View</p>
-				<img src='../Assets/photos/project4.jpg' alt="project-1" class="w-[275px] image rounded-lg" style="top: {top}px; left: {left}px;" transition:fade={{ delay: 150}}>
+				<img
+					src="../Assets/photos/project4.jpg"
+					alt="project-1"
+					class="w-[275px] image rounded-lg"
+					style="top: {top}px; left: {left}px;"
+					transition:fade={{ delay: 150 }}
+				/>
 			{/if}
 		</li>
-			{#if section4}
-				<div class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn" transition:slide>
-					<Section4/>
-					<button on:click={() => section4 = false} class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300">Hide</button>
-				</div>
-			{/if}
+		{#if section4}
+			<div
+				class="grid grid-cols-1 justify-items-center animate__animated animate__fadeIn"
+				in:slide
+				out:slide={{ delay: 600, duration: 700 }}
+			>
+				<Section4 />
+				<a
+					href="#section-4"
+					on:click|preventDefault={scrollIntoView}
+					on:click={() => (section4 = false)}
+					class="bg-blue-200 dark:bg-rose-400 justify-self-end rounded-full p-2 cursor-none hlink text-black hover:text-white transition ease-in-out duration-300"
+					>Hide</a
+				>
+			</div>
+		{/if}
 		<div class="divider bg-black dark:bg-white h-[1px] opacity-20" />
 	</ul>
 </div>
