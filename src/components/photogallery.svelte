2b712237a2b7c7676779bf3198bed037985a18c7
Piotrek <pepe.kazala@gmail.com>
2022-10-18 22:18:40 +0100

    Finished the Photography section

--- src/components/photogallery.svelte
+++ src/components/photogallery.svelte
@@ -87,31 +87,69 @@
 		<div class="slider absolute top-0 left-0 w-[3500px] h-full">
 			<div class="slider-inner absolute top-[50%] h-full w-full flex justify-around">
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/31.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/2.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/5.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/8.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/9.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/12.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/14.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/27.jpg" alt="" class="image w-96" />
 				</div>
 				<div class="item relative w-96 h-96 overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image w-96" />
+					<img src="/Images/1x1/25.jpg" alt="" class="image w-96" />
+				</div>
+			</div>
+		</div>
+	</section>
+	<section
+		class="relative overflow-hidden h-[32.4rem] max-w-7xl m-auto grid grid-cols-1 font-stylish text-black dark:text-white px-10 md:px-12 mine:px-14 lg:px-18 xlg:px-24 xl:px-[7.5rem]"
+	>
+		<div class="slider2 absolute top-0 left-0 w-[3889px] h-full">
+			<div class="slider-inner absolute top-[0%] h-full w-full flex justify-around">
+				<div class="item relative w-96 h-full overflow-hidden">
+					<img src="/Images/4x3/10.jpg" alt="" class="image2 w-96" />
+				</div>
+				<div class="item relative w-96 h-full overflow-hidden">
+					<img src="/Images/4x3/10.jpg" alt="" class="image2 w-96" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/20.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/17.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/30.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/29.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/33.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/6.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/26.jpg" alt="" class="image2 w-96 h-[34rem]" />
+				</div>
+				<div class="item relative w-96 h-[34rem] overflow-hidden">
+					<img src="/Images/4x3/5.jpg" alt="" class="image2 w-96 h-[34rem]" />
 				</div>
 			</div>
 		</div>
@@ -119,69 +157,43 @@
 	<section
 		class="relative overflow-hidden h-[24.4rem] max-w-7xl m-auto grid grid-cols-1 font-stylish text-black dark:text-white px-10 md:px-12 mine:px-14 lg:px-18 xlg:px-24 xl:px-[7.5rem]"
 	>
-		<div class="slider2 absolute top-0 left-0 w-[3500px] h-full">
+		<div class="slider3 absolute top-0 left-0 w-[4667px] h-full">
 			<div class="slider-inner absolute top-[1%] h-full w-full flex justify-around">
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/9.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/9.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/9.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/34.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/18.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/13.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/15.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
+					<img src="/Images/1x1/22.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image2 w-96" />
-				</div>
-			</div>
-		</div>
-	</section>
-	<section
-		class="relative overflow-hidden h-[24.4rem] max-w-7xl m-auto grid grid-cols-1 font-stylish text-black dark:text-white px-10 md:px-12 mine:px-14 lg:px-18 xlg:px-24 xl:px-[7.5rem]"
-	>
-		<div class="slider3 absolute top-0 left-0 w-[3500px] h-full">
-			<div class="slider-inner absolute top-[1%] h-full w-full flex justify-around">
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
+					<img src="/Images/1x1/16.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
+					<img src="/Images/1x1/2.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
+					<img src="/Images/1x1/23.jpg" alt="" class="image3 w-96" />
 				</div>
 				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
-				</div>
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
-				</div>
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
-				</div>
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
-				</div>
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
-				</div>
-				<div class="item relative w-96 h-full overflow-hidden">
-					<img src="/Images/23.jpg" alt="" class="image3 w-96" />
+					<img src="/Images/1x1/24.jpg" alt="" class="image3 w-96" />
 				</div>
 			</div>
 		</div>
